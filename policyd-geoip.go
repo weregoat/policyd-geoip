c77d544041d0ead6474601c070fc4362d12e33b1
Ruggero Cucchiani <ruggero.cucchiani@exertisztorm.com>
2018-04-09 14:41:57 +0200

    refactor functions
    Renamed _sysLog_ function to _sendToSyslog_
    Moved some code from _loadConfiguration_ in a separate function:
    _parseConfiguration_
    If Postfix doesn't send the "client_address" over, the program
    does not try to process the request, instead it will return
    a DEFER_IF_PERMIT;

--- policyd-geoip.go
+++ policyd-geoip.go
@@ -23,6 +23,8 @@ const Defer = "defer_if_permit"
 const DefaultGeoIP2Database = "/usr/share/GeoIP/GeoLite2-Country.mmdb"
 const DefaultConfigurationFile = "/etc/policyd-geoip.yaml"
 const DefaultRefreshInterval = "30m"
+const SyslogFacility = syslog.LOG_MAIL
+const SyslogTag = "policyd-geoip"
 
 var debug = false
 var blacklistedCountries []string
@@ -37,24 +39,27 @@ type Configuration struct {
 	GeoIP2Database string `yaml:"geoip2_database"`
 	RefreshInterval string `yaml:"refresh_interval"`
 	Whitelist []string `yaml:"whitelist"`
+	Facility string `yaml:"syslog_facility"`
+	Tag string `yaml:"syslog_tag"`
 }
 
 
 func main() {
 	start := time.Now()
-	sysLog(syslog.LOG_INFO, "program started")
+	sendToSyslog(syslog.LOG_INFO, "program started")
 	configuration := flag.String("configuration", DefaultConfigurationFile, "Path to the configuration")
 	flag.Parse()
 	loadConfiguration(*configuration)
 
-	response := Dunno
+	response := Defer
+
 	scanner := bufio.NewScanner(os.Stdin)
 	ip := ""
 	clientName := ""
 	for scanner.Scan() {
 		begin := time.Now()
 		line := scanner.Text()
-		sysLog(syslog.LOG_DEBUG, fmt.Sprintf("read line '%s' from stdin", line))
+		sendToSyslog(syslog.LOG_DEBUG, fmt.Sprintf("read line '%s' from stdin", line))
 		if strings.Contains(line, "=") {
 			values := strings.Split(scanner.Text(), "=")
 			key := values[0]
@@ -63,7 +68,7 @@ func main() {
 			case "client_address":
 				elapsed := time.Since(start)
 				if elapsed.Minutes() >= refreshInterval.Minutes() {
-					sysLog(syslog.LOG_INFO, "refreshing configuration")
+					sendToSyslog(syslog.LOG_INFO, "refreshing configuration")
 					loadConfiguration(*configuration)
 					start = time.Now()
 				}
@@ -74,19 +79,21 @@ func main() {
 		}
 		if scanner.Text() == "" {
 			if checkWhitelist(clientName) == false {
-				response = checkBlacklist(geoIP2Database, ip)
+				if len(ip) > 0 {
+					response = checkBlacklist(geoIP2Database, ip)
+				}
 			}
 			actionLine := fmt.Sprintf("action=%s",response)
-			sysLog(syslog.LOG_DEBUG, fmt.Sprintf("sending response '%s' to stdout", actionLine))
+			sendToSyslog(syslog.LOG_DEBUG, fmt.Sprintf("sending response '%s' to stdout", actionLine))
 			writer := bufio.NewWriter(os.Stdout)
 			writer.WriteString(fmt.Sprintf("%s\n\n",actionLine))
 			processDuration := time.Since(begin)
-			sysLog(syslog.LOG_INFO, fmt.Sprintf("processed in %s", processDuration.String()))
+			sendToSyslog(syslog.LOG_INFO, fmt.Sprintf("processed in %s", processDuration.String()))
 			writer.Flush()
 		}
 	}
 	if err := scanner.Err(); err != nil {
-		sysLog(syslog.LOG_ERR, err.Error())
+		sendToSyslog(syslog.LOG_ERR, err.Error())
 		fmt.Fprintln(os.Stderr, "reading standard input:", err)
 	}
 
@@ -94,9 +101,9 @@ func main() {
 
 }
 
-func sysLog(level syslog.Priority, message string) {
+func sendToSyslog(level syslog.Priority, message string) {
 
-	sysLog, err := syslog.Dial("", "", level|syslog.LOG_MAIL, "policyd-geoip")
+	sysLog, err := syslog.Dial("", "", level|SyslogFacility, SyslogTag)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -114,18 +121,18 @@ func checkBlacklist(database string, ipAddress string) string {
 	response := Defer
 	db, err := geoip2.Open(database)
 	if err != nil {
-		sysLog(syslog.LOG_ERR, err.Error())
+		sendToSyslog(syslog.LOG_ERR, err.Error())
 	} else {
 		defer db.Close()
 		ip := net.ParseIP(ipAddress)
 		if ip != nil {
 			record, err := db.Country(ip)
 			if err != nil {
-				sysLog(syslog.LOG_ERR, err.Error())
+				sendToSyslog(syslog.LOG_ERR, err.Error())
 			} else {
 				isoCode := record.Country.IsoCode
 				if isoCode != "" {
-					sysLog(syslog.LOG_DEBUG, fmt.Sprintf("geoIP2 database lists address %s as from country %s", ipAddress, isoCode))
+					sendToSyslog(syslog.LOG_DEBUG, fmt.Sprintf("geoIP2 database lists address %s as from country %s", ipAddress, isoCode))
 					response = Dunno
 					for _, blacklistedIsoCode := range blacklistedCountries {
 						if isoCode == blacklistedIsoCode {
@@ -137,15 +144,15 @@ func checkBlacklist(database string, ipAddress string) string {
 					if response == Blacklisted {
 						result = "not allowed"
 					}
-					sysLog(syslog.LOG_INFO, fmt.Sprintf("client with address %s from country %s is %s", ipAddress, isoCode, result))
+					sendToSyslog(syslog.LOG_INFO, fmt.Sprintf("client with address %s from country %s is %s", ipAddress, isoCode, result))
 
 				} else {
-					sysLog(syslog.LOG_NOTICE, fmt.Sprintf("no country found for address %s", ipAddress))
+					sendToSyslog(syslog.LOG_NOTICE, fmt.Sprintf("no country found for address %s", ipAddress))
 					response = Reject
 				}
 			}
 		} else {
-			sysLog(syslog.LOG_NOTICE, fmt.Sprintf("invalid client address '%s'", ipAddress))
+			sendToSyslog(syslog.LOG_NOTICE, fmt.Sprintf("invalid client address '%s'", ipAddress))
 			response = Reject
 		}
 	}
@@ -153,64 +160,29 @@ func checkBlacklist(database string, ipAddress string) string {
 }
 
 func loadConfiguration(configuration string) {
-	var blacklist []string
-	var whitelist []string
+
 	filename, err := filepath.Abs(configuration)
 	if err != nil {
-		sysLog(syslog.LOG_ERR,err.Error())
+		sendToSyslog(syslog.LOG_ERR,err.Error())
 	}
-	sysLog(syslog.LOG_INFO, fmt.Sprintf("importing configuration file %s", filename))
+	sendToSyslog(syslog.LOG_INFO, fmt.Sprintf("importing configuration file %s", filename))
 	yamlFile, err := ioutil.ReadFile(filename)
 	if err != nil {
-		sysLog(syslog.LOG_ERR,err.Error())
+		sendToSyslog(syslog.LOG_ERR,err.Error())
 	} else {
 		var config Configuration
 		err = yaml.Unmarshal(yamlFile, &config)
-		if err == nil {
-			for _, blacklistedCountry := range config.Blacklist {
-				isoCode := strings.TrimSpace(blacklistedCountry)
-				if len(isoCode) == 2 {
-					blacklist = append(blacklist, isoCode)
-				} else {
-					sysLog(syslog.LOG_WARNING, fmt.Sprintf("ignoring invalid string '%s' for ISO Country code", blacklistedCountry))
-				}
-			}
-			for _, whitelistedClient := range config.Whitelist {
-				clientName := strings.ToLower(strings.TrimSpace(whitelistedClient))
-				if len(clientName) > 0 && strings.Contains(clientName, "."){
-					whitelist = append(whitelist, clientName)
-				} else {
-					sysLog(syslog.LOG_WARNING, fmt.Sprintf("ignoring invalid string '%s' for client name", clientName))
-				}
-			}
-		}
-		blacklistedCountries = blacklist
-		whitelistedClients = whitelist
-		debug = config.Debug
-		databaseFile, err := filepath.Abs(config.GeoIP2Database)
 		if err != nil {
-			sysLog(syslog.LOG_WARNING, err.Error())
+			sendToSyslog(syslog.LOG_ERR, err.Error())
 		} else {
-			_, err := os.Stat(databaseFile)
-			if err == nil {
-				geoIP2Database = databaseFile
-			} else {
-				sysLog(syslog.LOG_WARNING, err.Error())
-			}
+			parseConfiguration(config)
 		}
-		interval, err := time.ParseDuration(config.RefreshInterval)
-		if err != nil {
-			sysLog(syslog.LOG_WARNING, err.Error())
-		} else {
-			refreshInterval = interval
-		}
-
 	}
-	sysLog(syslog.LOG_INFO, fmt.Sprintf("debug: %v", debug))
-	sysLog(syslog.LOG_INFO, fmt.Sprintf("blacklisted countries: %v", blacklistedCountries))
-	sysLog(syslog.LOG_INFO, fmt.Sprintf("geoip2 database: %v", geoIP2Database))
-	sysLog(syslog.LOG_INFO, fmt.Sprintf("refresh time: %s", refreshInterval.String()))
-	sysLog(syslog.LOG_INFO, fmt.Sprintf("whitelisted clients: %v", whitelistedClients))
+	sendToSyslog(syslog.LOG_INFO, fmt.Sprintf("debug: %v", debug))
+	sendToSyslog(syslog.LOG_INFO, fmt.Sprintf("blacklisted countries: %v", blacklistedCountries))
+	sendToSyslog(syslog.LOG_INFO, fmt.Sprintf("geoip2 database: %v", geoIP2Database))
+	sendToSyslog(syslog.LOG_INFO, fmt.Sprintf("refresh time: %s", refreshInterval.String()))
+	sendToSyslog(syslog.LOG_INFO, fmt.Sprintf("whitelisted clients: %v", whitelistedClients))
 }
 
 func checkWhitelist(clientName string) bool {
@@ -218,10 +190,55 @@ func checkWhitelist(clientName string) bool {
 	for _,allowedClient := range whitelistedClients {
 		match := strings.HasSuffix(strings.ToLower(strings.TrimSpace(clientName)), allowedClient)
 		if match == true {
-			sysLog(syslog.LOG_INFO, fmt.Sprintf("client %s is whitelisted under %s", clientName,allowedClient))
+			sendToSyslog(syslog.LOG_INFO, fmt.Sprintf("client %s is whitelisted under %s", clientName,allowedClient))
 			allow = true
 			break
 		}
 	}
 	return allow
 }
+
+func parseConfiguration(config Configuration) {
+
+	for _, blacklistedCountry := range config.Blacklist {
+		isoCode := strings.TrimSpace(blacklistedCountry)
+		if len(isoCode) == 2 {
+			blacklistedCountries = append(blacklistedCountries, isoCode)
+		} else {
+			sendToSyslog(syslog.LOG_WARNING, fmt.Sprintf("ignoring invalid string '%s' for ISO Country code", blacklistedCountry))
+		}
+	}
+
+	for _, whitelistedClient := range config.Whitelist {
+		clientName := strings.ToLower(strings.TrimSpace(whitelistedClient))
+		if len(clientName) > 0 && strings.Contains(clientName, "."){
+			whitelistedClients = append(whitelistedClients, clientName)
+		} else {
+			sendToSyslog(syslog.LOG_WARNING, fmt.Sprintf("ignoring invalid string '%s' for client name", clientName))
+		}
+	}
+
+	debug = config.Debug
+
+	databaseFile, err := filepath.Abs(config.GeoIP2Database)
+	if err != nil {
+		sendToSyslog(syslog.LOG_WARNING, err.Error())
+	} else {
+		_, err := os.Stat(databaseFile)
+		if err == nil {
+			geoIP2Database = databaseFile
+		} else {
+			sendToSyslog(syslog.LOG_WARNING, err.Error())
+		}
+	}
+
+	interval, err := time.ParseDuration(config.RefreshInterval)
+	if err != nil {
+		sendToSyslog(syslog.LOG_WARNING, err.Error())
+	} else {
+		refreshInterval = interval
+	}
+
+}
+
+
